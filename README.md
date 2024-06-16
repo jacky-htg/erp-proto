b2bae4303bc13bd330b0134c95620033aedf2f94
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2024-06-16 17:31:06 +0700

    access adjustement

---
+++ README.md
@@ -0,0 +1,2 @@
+# erp-proto
+a proto file and genereate code in javascript for ERP project
---
+++ pb/users/access_service_pb.js
@@ -0,0 +1,27 @@
+// source: users/access_service.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global =
+    (typeof globalThis !== 'undefined' && globalThis) ||
+    (typeof window !== 'undefined' && window) ||
+    (typeof global !== 'undefined' && global) ||
+    (typeof self !== 'undefined' && self) ||
+    (function () { return this; }).call(null) ||
+    Function('return this')();
+
+var users_access_message_pb = require('../users/access_message_pb.js');
+goog.object.extend(proto, users_access_message_pb);
+var users_generic_message_pb = require('../users/generic_message_pb.js');
+goog.object.extend(proto, users_generic_message_pb);
