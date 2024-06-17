e9977fbfdc89be40fcb4ab7d71787919cb9e9eef
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2024-06-17 20:39:36 +0700

    inventories service

---
+++ pb/inventories/category_service_pb.js
@@ -0,0 +1,27 @@
+// source: inventories/category_service.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global =
+    (typeof globalThis !== 'undefined' && globalThis) ||
+    (typeof window !== 'undefined' && window) ||
+    (typeof global !== 'undefined' && global) ||
+    (typeof self !== 'undefined' && self) ||
+    (function () { return this; }).call(null) ||
+    Function('return this')();
+
+var inventories_category_message_pb = require('../inventories/category_message_pb.js');
+goog.object.extend(proto, inventories_category_message_pb);
+var inventories_generic_message_pb = require('../inventories/generic_message_pb.js');
+goog.object.extend(proto, inventories_generic_message_pb);
